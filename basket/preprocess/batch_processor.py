b95c413f94df598f72bb3cba0b72dbfc8be4d026
MuhHanif <48muhhanif@gmail.com>
2023-03-31 17:20:43 +0700

    stream_data functionality

--- basket/preprocess/batch_processor.py
+++ basket/preprocess/batch_processor.py
@@ -1,4 +1,7 @@
 from typing import Union, Callable
+from dataclasses import dataclass
+from multiprocessing.dummy import Pool
+
 import PIL
 from PIL import ImageFile, Image
 import pandas as pd
@@ -9,11 +12,12 @@ from io import BytesIO
 from transformers import CLIPTokenizer
 
 
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def stream_image(url:str, threshold_size:int=512, debug:bool=True) -> Image:
     r"""
-    stream image from internet given url
+    stream image from internet
     
     args:
         url (:obj:`str`):
@@ -306,3 +310,192 @@ def generate_batch(
     output["attention_mask"] = tokenizer_dict.attention_mask
 
     return output
+
+@dataclass
+class pil_image:
+    image: Image
+        
+# TODO: docstring
+def stream_data(
+    df:pd.DataFrame,
+    batch_size:int,
+    url_col:str,
+    width_col:str,
+    height_col:str,
+    text_col:str,
+    hash_col:str,
+    tokenizer:CLIPTokenizer,
+    caption_token_length:int,
+    batch_slice:int=1,
+    min_res_axis:int = 512,
+    debug:bool = True,
+    check_col:int = "downloaded_at_batch",
+    stream_image_fn:Callable[[str, int, bool], pil_image] = stream_image,
+    process_image_fn:Callable[[str, tuple], np.array] = process_image,
+    tokenize_text_fn:Callable[[str, str, int], dict] = tokenize_text,
+    
+) -> Union[dict, dict, pd.DataFrame]:
+    
+    # NOTE!
+    # dataframe must come in pre shuffled!
+    # dataframe must come in with the same resolution bucket!
+    # could add some assertion check
+    # width and height col must be a bucketing size
+    
+    # TODO:
+    # add terminate signal to signal the training loop to stop training and save the model
+    # threading download
+    
+    # assigning a new column that marks
+    # if it's already downloaded or not
+    # -1 is marked to download
+    # -2 is failed
+    # > 0 is batch number
+    if not check_col in df.columns.tolist():
+        df[check_col] = -1
+        if debug:
+            print("dataframe does not contain prior `check_col` creating one")
+    elif debug:
+        print("dataframe already has `check_col` in it")
+
+    # select rows that yet to be processed
+    selected_df = df[df[check_col]==-1]
+    unselected_df = df[df[check_col]!=-1]
+    
+    # keep track of the previous batch retrieval
+    max_check_col_value = df[check_col].max()
+       
+    # batch size == numb of worker
+    # strip the tail end so each worker have equal dataframe
+    # only if it's not divisible by worker count
+    if len(selected_df) % batch_size > 0:
+        
+        remainder_selected_df = selected_df.iloc[-(len(selected_df) % batch_size):] # modulo return 0 bug!
+        selected_df = selected_df.iloc[:-(len(selected_df) % batch_size)]
+        
+    else:
+        remainder_selected_df = pd.DataFrame()
+    
+    # store chunks here
+    chunked_df = [] 
+    chunk_length = len(selected_df) // batch_size    
+    
+    # this is the loop to break up the dataframe into chunks
+    # divide the dataframe equally for each workers
+    # [1,2,...,end] to
+    # [1,2,...n][n+1,n+2,...m][m+1,m+2,...,end]   
+    for x in range(batch_size):
+        start = chunk_length * x
+        end = start + chunk_length
+        chunk = selected_df.iloc[start:end]
+        chunked_df.append(chunk)
+            
+    # concurrent stream 
+    def _stream_process(df:pd.DataFrame):
+        # store streamed data here
+        streamed_data = [] 
+        
+        # keep trying until the batch size is met
+        counter = 0
+        
+        # separating image streaming loop from processing loop
+        while len(streamed_data) == 0:
+
+            selected_row = df.iloc[counter]
+            # image url to retrieve from
+            image_url = selected_row[url_col]
+            
+            # bucket width and height
+            width_height = [
+                selected_row[width_col],
+                selected_row[height_col],
+            ]
+            
+            # get image from the url and store it as 
+            # a pil image object, preparing conversion to numpy
+            image = stream_image_fn(
+                url=image_url, 
+                threshold_size=min_res_axis,
+                debug=debug
+            )
+            
+            # accosiated text from the image for the tokenizer
+            text = selected_row[text_col],
+            
+            # get hash column (or any unique col) to be associated with image
+            # usefull for storing the image while streaming
+            hash_value = selected_row[hash_col],
+            
+            # only increment if succeed!
+            if image != None:
+                
+                # store everything here
+                image_text_pair = {
+                    "image":image.convert('RGB'), # remove alpha channel 
+                    "text":text, 
+                    "hash":hash_value, 
+                    "size":width_height
+                }
+                streamed_data.append(image_text_pair)
+                
+                # if succeed write the check col to keep track the order
+                # for replicability purposes i hope
+                df.iloc[counter, df.columns.get_loc(check_col)] = max_check_col_value + 1
+            
+            else: 
+                # i think i should add error column to know 
+                # why it couldn't get the image
+                df.iloc[counter, df.columns.get_loc(check_col)] = -2
+                
+            counter = counter + 1
+
+        # this is convoluted but my mind is hazy rn so let it be
+        # returning both data dict and dataframe to be concatenated for the next step
+        return (streamed_data[0], df) 
+    
+    
+    with Pool(processes=len(chunked_df)) as pool:
+        # Use the pool to process the tasks concurrently for the entire batch
+        concurrent_stream = pool.map(_stream_process, chunked_df)
+    
+    # unpack output
+    batch_image, chunked_df = zip(*concurrent_stream)
+    
+    ##processing batch##
+    
+    # store all numpy images & text to be concatenated here
+    np_image_batch = []
+    text_batch = []
+    
+    for streamed_data in batch_image:
+
+        # grab iamge from path and then process it
+        image = process_image_fn(image=streamed_data["image"], rescale_size=streamed_data["size"])
+        text = streamed_data["text"]
+        
+        np_image_batch.append(image)
+        text_batch.append(text[0])
+        
+    # stack image into neat array 
+    np_image_batch = np.stack(np_image_batch)
+    # placeboo but i think it feels faster lol
+    np_image_batch = np.ascontiguousarray(np_image_batch)
+    
+    # tokenize text into integer representing the text
+    tokenizer_dict = tokenize_text_fn(
+        tokenizer=tokenizer, 
+        text_prompt=text_batch, 
+        max_length=caption_token_length, 
+        batch_slice=batch_slice
+    )
+    
+    output = {
+        "pixel_values":np_image_batch,
+        "input_ids":tokenizer_dict.input_ids,
+        "attention_mask":tokenizer_dict.attention_mask
+    }
+            
+    # recombine dataframe again for the next iteration
+    reconcat_df = pd.concat( list(chunked_df) + [remainder_selected_df, unselected_df])
+    
+    return(batch_image, output, reconcat_df)
